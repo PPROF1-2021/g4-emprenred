95888d67ddc057754cab1120c239d702e3198eba
CelesteMiranda <celestemiranda.at@gmail.com>
2021-11-12 20:58:35 -0300

    validaciones js

--- src/main/resources/static/js/registro/provincias.js
+++ src/main/resources/static/js/registro/provincias.js
@@ -27,15 +27,33 @@ function addOptions(domElement, array) {
 
 // validacion form registro
 
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
+const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/, // 7 a 14 numeros.
+};
+const campos = {
+    usuario: false,
+    nombre: false,
+    password: false,
+    correo: false,
+    telefono: false,
+};
+
 function validar() {
     var nombre, apellido, provincias, fechaNac, email, password, password2;
     nombre = document.getElementById("nombre");
-    apellido = document.getElementById("apellido").value;
-    provincias = document.getElementById("provincia").value;
-    fechaNac = document.getElementById("nombre").value;
-    email = document.getElementById("email").value;
-    password = document.getElementById("password").value;
-    password2 = document.getElementById("passwprd2").value;
+    apellido = document.getElementById("apellido");
+    provincias = document.getElementById("provincia");
+    fechaNac = document.getElementById("nombre");
+    email = document.getElementById("email");
+    password = document.getElementById("password");
+    password2 = document.getElementById("passwprd2");
     var nombreDiv = document.getElementById("nombreDiv");
     let p = document.createElement("p");
 
@@ -45,3 +63,18 @@ function validar() {
         return false;
     }
 }
+
+function checklargo(campo, min) {
+    var palabra = campo.value;
+    // Crear mensaje, en blanco para valor correcto
+    let validity = campo.validity;
+    var msg = "";
+
+    if (validity.tooShort) {
+        alert("muy corto");
+        //msg = "Excede el largo permitido";
+    }
+    campo.reportValidty();
+}
+
+formulario.addEventListener("submit", validar);
