20905e3be1a84d4e3b617e9d4774bf8d196eefcf
CelesteMiranda <celestemiranda.at@gmail.com>
2021-11-14 23:08:39 -0300

    arreglos finales

--- src/main/resources/templates/envio-contacto.php
+++ src/main/resources/templates/envio-contacto.php
@@ -140,10 +140,14 @@ $consulta ="INSERT INTO personas(nombre,apellido,provincia,fechaNacimiento,email
 
   </footer>
 <script>
-  setTimeout(function () {
+ 
+
+ alert("se proceso correctamente sus datos");
+
+setTimeout(function () {
    window.location.href= 'login.html';},5000); 
 </script>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 </html>
